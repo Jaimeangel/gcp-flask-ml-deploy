a3d893796ad0f4c4c45931f630a48d44d6caae01
Jaimeangel <69481530+Jaimeangel@users.noreply.github.com>
2024-01-13 23:17:01 -0500

    testing CD2

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def hello():
     """Return a friendly HTTP greeting."""
     print("I am inside hello world")
-    return 'Hello World! CD from JAIME-ANGEL repo'
+    return 'Hello World! CD from JAIME-ANGEL repository'
 
 @app.route('/echo/<name>')
 def echo(name):
